97df5466ba358dd3c4b829b5eb6dabd3b5ca2944
Cecy Perez <cecilia_perez03@hotmal.com>
2020-09-21 18:49:01 -0500

    Docs agregados

---
+++ docs/precache-manifest.b9150b93bc731e9f20e90819e732c633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a383d2fd5728ea7219f2e1582f70fd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad200a76786ec1c432b7",
+    "url": "/static/css/main.20f081e4.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad200a76786ec1c432b7",
+    "url": "/static/js/main.f948e753.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
